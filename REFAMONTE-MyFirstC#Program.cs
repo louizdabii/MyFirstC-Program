9864629a56599fe4c3244d3025f4867204ef509d
Louise Anne D. Refamonte <louise.refamonte@jmc.edu.ph>
2025-01-21 16:07:00 +0800

    Add files via upload

---
+++ REFAMONTE-MyFirstC#Program.cs
@@ -0,0 +1,22 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+class SampleProgram
+{
+    public static void Main(string[] args)
+    {
+        //Data Types
+        string message = "Touya Todoroki";
+        int num = 0118;
+        float pi = 3.14f;
+        char letter = 'D';
+        bool flag = true; //false or true
+        
+        Console.WriteLine("I love Dabi"); // output
+        Console.Write("I love Touya Todoroki: "); // output
+        string myinput = Console.ReadLine(); // input
+        Console.WriteLine(myinput);
+    }
+}
